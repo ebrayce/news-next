b6ea9808333787a767adcb3b7dc92d7c42ba677f
Ernest Brayce <gamiskah@gmail.com>
2025-01-12 22:21:01 +0000

    fix: correct types

--- src/app/news/[id]/[newsTitle]/page.tsx
+++ src/app/news/[id]/[newsTitle]/page.tsx
@@ -4,16 +4,16 @@ const prisma = new PrismaClient()
 
 type NewsProps = {
   id: string
-  title: string
-  content: string
-  author: string
-  description: string
-  imgurl: string
-  publishedAt: Date
-  categoryId: string
+  title: string|null
+  content: string|null
+  author: string|null
+  description: string|null
+  imgurl: string|null
+  publishedAt: Date|null
+  categoryId: string|null
 } | null
 
-const getNewsById = async (id: string) => {
+const getNewsById = async (id: string): Promise<NewsProps|null> => {
   return prisma.news.findUnique({
     where: { id: id },
   })
@@ -28,7 +28,7 @@ export default async function Page({
 }) {
   const { newsTitle, id } = await params
 
-  const news: NewsProps = await getNewsById(id)
+  const news = await getNewsById(id)
   console.log(id, news, searchParams, newsTitle)
 
   if (!news) {
@@ -41,8 +41,8 @@ export default async function Page({
       <p>{news.content}</p>
       <p>Author: {news.author}</p>
       <p>Description: {news.description}</p>
-      <img src={news.imgurl} alt={news.title} />
-      <p>Published at: {new Date(news.publishedAt).toLocaleDateString()}</p>
+      {news.imgurl && <img src={news.imgurl} alt={news.title || 'News image'} />}
+      <p>Published at: {news.publishedAt && new Date(news.publishedAt).toLocaleDateString() || 'No Idea'}</p>
     </div>
   )
 }
