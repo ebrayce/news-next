7504306324ea7e4c982b3ba41cbd8ac828cb53e6
Ernest Brayce <gamiskah@gmail.com>
2025-01-14 22:06:50 +0000

    feat: add author

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require('@prisma/client')
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://localhost:3000', // Site domain. Do not include a trailing slash!
@@ -16,7 +16,7 @@ module.exports = {
         id: true,
         name: true,
       },
-    });
+    })
 
     // Fetch news dynamically from the database
     const newsItems = await prisma.news.findMany({
@@ -24,29 +24,29 @@ module.exports = {
         id: true,
         title: true,
       },
-    });
+    })
 
     // Generate home page paths
     const homePagePaths = [
       { loc: '/', changefreq: 'daily', priority: 1.0 }, // Home page without query
       { loc: '/?query=heloo', changefreq: 'daily', priority: 0.9 }, // Home page with query
-    ];
+    ]
 
     // Generate category paths for each category
     const categoryPaths = categories.map((category) => ({
       loc: `/${category.name.toLowerCase()}`, // Example: /business, /sports, etc.
       changefreq: 'daily',
       priority: 0.8,
-    }));
+    }))
 
     // Generate news paths for each news item
     const newsPaths = newsItems.map((news) => ({
       loc: `/news/${news.id}/${news.title.replace(/\s+/g, '-').toLowerCase()}`, // Dynamic news path
       changefreq: 'daily',
       priority: 0.7,
-    }));
+    }))
 
     // Return all paths to be included in the sitemap
-    return [...homePagePaths, ...categoryPaths, ...newsPaths];
+    return [...homePagePaths, ...categoryPaths, ...newsPaths]
   },
-};
+}
