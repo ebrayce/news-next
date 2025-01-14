50064d522f48d00def3c3b1d779ed12f2de47a62
Ernest Brayce <gamiskah@gmail.com>
2025-01-14 02:08:11 +0000

    chore: lint files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,11 @@ export default async function Home({
       <NavBar currentPage="" currentQuery={currentQuery as string} />
 
       <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        {currentQuery && <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 '><SearchResultNews query={currentQuery as string} /></div>}
+        {currentQuery && (
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 ">
+            <SearchResultNews query={currentQuery as string} />
+          </div>
+        )}
 
         {!currentQuery &&
           (await Promise.all(
